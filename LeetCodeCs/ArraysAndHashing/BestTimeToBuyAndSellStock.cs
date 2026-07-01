f23a340ac9b8ad85bcc1fb0cd16de447f414b9fb
nikitareshetnik <reshetnik.nikita@gmail.com>
2026-07-01 10:57:00 +0300

    Solved https://leetcode.com/problems/best-time-to-buy-and-sell-stock

---
+++ LeetCodeCs/ArraysAndHashing/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,23 @@
+namespace LeetCodeCs.ArraysAndHashing;
+
+public static partial class Problem
+{
+    public static int MaxProfit(int[] prices) {
+        var profit = 0;
+
+        var buy = prices[0];
+
+        for (var i = 1; i < prices.Length; i++){
+            var sell = prices[i];
+            if(sell > buy){
+                var diff = sell - buy;
+                profit = profit > diff ? profit : diff;
+            }
+            else{
+                buy = sell;
+            }
+        }
+
+        return profit;
+    }
+}

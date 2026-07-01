1ac7ad5fa72bc34141e6e993539ed41308a5809c
nikitareshetnik <reshetnik.nikita@gmail.com>
2026-07-01 11:04:04 +0300

    Solved https://leetcode.com/problems/longest-palindromic-substring

---
+++ LeetCodeCs/TwoPointers/LongestPalindromicSubstring.cs
@@ -0,0 +1,40 @@
+namespace LeetCodeCs.TwoPointers;
+
+public static partial class Problem
+{
+    public static string LongestPalindrome(string s) {
+        if (string.IsNullOrEmpty(s)) return "";
+        int bestStart = 0, bestEnd = 0;
+
+        for(var i = 0; i < s.Length; i++){
+            // Odd
+            int left = i, right = i;
+
+            while(left >= 0 && right < s.Length && s[left] == s[right]){
+                if((right - left + 1) > (bestEnd - bestStart)){
+                    bestStart = left;
+                    bestEnd = right + 1;
+                }
+
+                left--;
+                right++;
+            }
+
+            // Even
+            left = i;
+            right = i + 1;
+
+            while(left >= 0 && right < s.Length && s[left] == s[right]){
+                if((right - left + 1) > (bestEnd - bestStart)){
+                    bestStart = left;
+                    bestEnd = right + 1;
+                }
+
+                left--;
+                right++;
+            }
+        }
+
+        return s[bestStart..bestEnd];
+    }
+}
